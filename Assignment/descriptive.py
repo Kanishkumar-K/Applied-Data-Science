341b7c9b230b112c4524c443241b1105faec97ed
Kanishkumar <kanishkumar.24cs@licet.ac.in>
2023-05-09 12:39:54 +0530

    Delete Assignment directory

--- Assignment/descriptive.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-# Load the dataset into a pandas dataframe
-df = pd.read_csv('house_price_dataset_of_india.csv')
-
-# Descriptive Statistics
-# Calculate descriptive statistics
-descriptive_stats = df.describe()
-print(descriptive_stats)
